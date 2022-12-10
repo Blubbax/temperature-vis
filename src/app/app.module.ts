326a46632a26a97d22b1c7cae164ba5527f247d0
Nico Pistor <nico.pistor@hs-furtwangen.de>
2022-12-11 00:44:23 +0100

    Add data and generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './visualizations/map/map.component';
+import { LinechartComponent } from './visualizations/linechart/linechart.component';
+import { BoxplotchartComponent } from './visualizations/boxplotchart/boxplotchart.component';
+import { TableComponent } from './visualizations/table/table.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { VisualizationsComponent } from './components/visualizations/visualizations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    LinechartComponent,
+    BoxplotchartComponent,
+    TableComponent,
+    HeaderComponent,
+    FooterComponent,
+    FilterComponent,
+    VisualizationsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/visualizations/boxplotchart/boxplotchart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxplotchart',
+  templateUrl: './boxplotchart.component.html',
+  styleUrls: ['./boxplotchart.component.scss']
+})
+export class BoxplotchartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
