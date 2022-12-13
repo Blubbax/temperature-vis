24de9a9d3486acd30431e84c4c102223cbccc344
Nico Pistor <nico.pistor@hs-furtwangen.de>
2022-12-13 16:03:59 +0100

    boxplot data

--- src/app/visualizations/boxplotchart/boxplotchart.component.ts
+++ src/app/visualizations/boxplotchart/boxplotchart.component.ts
@@ -1,8 +1,9 @@
-import { DataService } from './../../service/data.service';
 import { Station } from './../../model/station';
+import { TemperatureRecord } from './../../model/temperature-record';
+import { DataService } from './../../service/data.service';
 import { Component, OnInit } from '@angular/core';
 
-declare function drawBoxPlotChart(data: Station[]): void;
+declare function drawBoxPlotChart(data: TemperatureRecord[]): void;
 
 @Component({
   selector: 'app-boxplotchart',
@@ -14,8 +15,8 @@ export class BoxplotchartComponent implements OnInit {
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-    this.dataService.stationsVisSelection.subscribe(data => {
-      drawBoxPlotChart(data);
+    this.dataService.stationsVisSelection.subscribe((data: Station[]) => {
+      drawBoxPlotChart(this.dataService.getStationsAsTemperatureList([data[0]]));
     });
   }
 
--- src/assets/d3-scripts/boxplotchart.js
+++ src/assets/d3-scripts/boxplotchart.js
@@ -1,12 +1,26 @@
 boxPlotChartCreated = false;
 
-function drawBoxPlotChart(datassas) {
+function resizeBoxPlotChart() {
+  var svg = d3.select("div#boxplot-visualization").select('svg');
 
+  var width = parseInt(d3.select("div#boxplot-visualization").style('width'), 10)
+  var height = parseInt(d3.select("div#boxplot-visualization").style('height'), 10)
+
+  svg.attr("width", width)
+    .attr("height", height);
+}
+
+function drawBoxPlotChart(data) {
+  // inspired by https://d3-graph-gallery.com/graph/boxplot_show_individual_points.html
 
   // set the dimensions and margins of the graph
-  var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+  var margin = { top: 10, right: 30, bottom: 30, left: 40 };
+    // width = 800 - margin.left - margin.right,
+    // height = 400 - margin.top - margin.bottom;
+
+    var width = parseInt(d3.select("div#boxplot-visualization").style('width'), 10) - margin.left - margin.right;
+    var height = parseInt(d3.select("div#boxplot-visualization").style('height'), 10) - margin.top - margin.bottom;
+
 
   // append the svg object to the body of the page
   var svg = d3.select("div#boxplot-visualization")
@@ -17,94 +31,91 @@ function drawBoxPlotChart(datassas) {
     .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")");
 
-  // Read the data and compute summary statistics for each species
-  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv").then(function (data) {
+  // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
+  // var sumstat = d3.group(data, d => d.Species) // nest function allows to group the calculation per level of a factor
+  var sumstat = d3.rollup(data, d => {
+    console.log("Here comes d")
+    console.log(d);
+    q1 = d3.quantile(d.map(function (g) { return g.temperature; }).sort(d3.ascending), .25);
+    median = d3.quantile(d.map(function (g) { return g.temperature; }).sort(d3.ascending), .5);
+    q3 = d3.quantile(d.map(function (g) { return g.temperature; }).sort(d3.ascending), .75);
+    interQuantileRange = q3 - q1;
+    min = q1 - 1.5 * interQuantileRange;
+    max = q3 + 1.5 * interQuantileRange;
+    return ({ q1: q1, median: median, q3: q3, interQuantileRange: interQuantileRange, min: min, max: max })
+  }, d => d.month)
 
-    // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
-    // var sumstat = d3.group(data, d => d.Species) // nest function allows to group the calculation per level of a factor
-    var sumstat = d3.rollup(data, d => {
-      console.log("Here comes d")
-      console.log(d);
-      q1 = d3.quantile(d.map(function (g) { return g.Sepal_Length; }).sort(d3.ascending), .25);
-      median = d3.quantile(d.map(function (g) { return g.Sepal_Length; }).sort(d3.ascending), .5);
-      q3 = d3.quantile(d.map(function (g) { return g.Sepal_Length; }).sort(d3.ascending), .75);
-      interQuantileRange = q3 - q1;
-      min = q1 - 1.5 * interQuantileRange;
-      max = q3 + 1.5 * interQuantileRange;
-      return({q1: q1, median: median, q3: q3, interQuantileRange: interQuantileRange, min: min, max: max})
-    }, d => d.Species)
+  console.log("here comes the final dataset")
+  console.log(sumstat);
 
-    console.log(sumstat);
+  // Show the X scale
+  var x = d3.scaleBand()
+    .range([0, width])
+    .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    .paddingInner(1)
+    .paddingOuter(.5)
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
 
-    // Show the X scale
-    var x = d3.scaleBand()
-      .range([0, width])
-      .domain(["setosa", "versicolor", "virginica"])
-      .paddingInner(1)
-      .paddingOuter(.5)
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
+  // Show the Y scale
+  var y = d3.scaleLinear()
+    .domain([d3.min(data, function (d) { return +d.temperature; }) - 5, d3.max(data, function (d) { return +d.temperature; }) + 5])
+    .range([height, 0])
+  svg.append("g").call(d3.axisLeft(y))
 
-    // Show the Y scale
-    var y = d3.scaleLinear()
-      .domain([3, 9])
-      .range([height, 0])
-    svg.append("g").call(d3.axisLeft(y))
+  // Show the main vertical line
+  svg
+    .selectAll("vertLines")
+    .data(sumstat)
+    .enter()
+    .append("line")
+    .attr("x1", function (d) { return (x(d[0])) })
+    .attr("x2", function (d) { return (x(d[0])) })
+    .attr("y1", function (d) { return (y(d[1].min)) })
+    .attr("y2", function (d) { return (y(d[1].max)) })
+    .attr("stroke", "black")
+    .style("width", 40)
 
-    // Show the main vertical line
-    svg
-      .selectAll("vertLines")
-      .data(sumstat)
-      .enter()
-      .append("line")
-      .attr("x1", function (d) { return (x(d[0])) })
-      .attr("x2", function (d) { return (x(d[0])) })
-      .attr("y1", function (d) { return (y(d[1].min)) })
-      .attr("y2", function (d) { return (y(d[1].max)) })
-      .attr("stroke", "black")
-      .style("width", 40)
+  // rectangle for the main box
+  var boxWidth = 50
+  svg
+    .selectAll("boxes")
+    .data(sumstat)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) { return (x(d[0]) - boxWidth / 2) })
+    .attr("y", function (d) { return (y(d[1].q3)) })
+    .attr("height", function (d) { return (y(d[1].q1) - y(d[1].q3)) })
+    .attr("width", boxWidth)
+    .attr("stroke", "black")
+    .style("fill", "#69b3a2")
 
-    // rectangle for the main box
-    var boxWidth = 100
-    svg
-      .selectAll("boxes")
-      .data(sumstat)
-      .enter()
-      .append("rect")
-      .attr("x", function (d) { return (x(d[0]) - boxWidth / 2) })
-      .attr("y", function (d) { return (y(d[1].q3)) })
-      .attr("height", function (d) { return (y(d[1].q1) - y(d[1].q3)) })
-      .attr("width", boxWidth)
-      .attr("stroke", "black")
-      .style("fill", "#69b3a2")
+  // Show the median
+  svg
+    .selectAll("medianLines")
+    .data(sumstat)
+    .enter()
+    .append("line")
+    .attr("x1", function (d) { return (x(d[0]) - boxWidth / 2) })
+    .attr("x2", function (d) { return (x(d[0]) + boxWidth / 2) })
+    .attr("y1", function (d) { return (y(d[1].median)) })
+    .attr("y2", function (d) { return (y(d[1].median)) })
+    .attr("stroke", "black")
+    .style("width", 80)
 
-    // Show the median
-    svg
-      .selectAll("medianLines")
-      .data(sumstat)
-      .enter()
-      .append("line")
-      .attr("x1", function (d) { return (x(d[0]) - boxWidth / 2) })
-      .attr("x2", function (d) { return (x(d[0]) + boxWidth / 2) })
-      .attr("y1", function (d) { return (y(d[1].median)) })
-      .attr("y2", function (d) { return (y(d[1].median)) })
-      .attr("stroke", "black")
-      .style("width", 80)
-
-    // Add individual points with jitter
-    var jitterWidth = 50
-    svg
-      .selectAll("indPoints")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d) { return (x(d.Species) - jitterWidth / 2 + Math.random() * jitterWidth) })
-      .attr("cy", function (d) { return (y(d.Sepal_Length)) })
-      .attr("r", 4)
-      .style("fill", "white")
-      .attr("stroke", "black")
+  // Add individual points with jitter
+  var jitterWidth = 20
+  svg
+    .selectAll("indPoints")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return (x(d.month) - jitterWidth / 2 + Math.random() * jitterWidth) })
+    .attr("cy", function (d) { return (y(d.temperature)) })
+    .attr("r", 4)
+    .style("fill", "white")
+    .attr("stroke", "black")
 
 
-  })
 }
